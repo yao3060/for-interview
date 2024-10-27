2d53c50bf1115ece8c14d6ff81c92e02cfd226c6
YAOYINGYING <yao3060@gmail.com>
2024-10-27 21:57:46 +0800

    Update and rename get_deep_value.php to array_get.php

---
+++ array_get.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * retrieve a value from a deeply nested array using "dot" notation
+ */
+function array_get($array, $path, $default="")  {
+
+}
+
+
+$product = [
+	"id" => 1,
+	"title" => "iPhone 16",
+	"price" => {
+		"default" => 5999,
+		"pro" => 6999,
+		"pro_max" => [
+			"_256" => 7999,
+			"_512" => 8999
+		]
+	},
+];
+
+// example usage
+array_get($product, 'not.exist'); // expect "" 
+array_get($product, 'price.pro'); // expect: 6999
+array_get($product, 'price.pro_max._512'); // expect: 8999 
